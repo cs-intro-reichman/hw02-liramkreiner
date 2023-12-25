31687355ac3555bd2031e1c1fcf5879bf6d8e3da
liram kreiner <liram.kreiner@post.runi.ac.il>
2023-12-25 19:13:04 +0200

    edit perfect file

--- Perfect.java
+++ Perfect.java
@@ -4,5 +4,20 @@
 public class Perfect {
 	public static void main (String[] args) {
 		//// Put your code here
+		int sum = 0 , thedeviser = 1;
+		int theperfctumber = Integer.parseInt(args[0]);
+		String sequence = "";
+		while (thedeviser<theperfctumber)
+		{
+			if(theperfctumber%thedeviser == 0)
+			{
+				sum += thedeviser;
+				sequence += thedeviser + " + ";
+			}
+			thedeviser++;
+		}
+		if(sum == theperfctumber)
+			System.out.println( theperfctumber+" is a perfect number since " + sequence + "= " + theperfctumber);
+		else System.out.println(theperfctumber + " is not a perfect number");
 	}
 }
