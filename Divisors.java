391f2bbcc5b699f2e8cc0b57b7e1d1cc62cd6ef1
liram kreiner <liram.kreiner@post.runi.ac.il>
2023-12-25 17:50:42 +0200

    update my two files

--- Divisors.java
+++ Divisors.java
@@ -2,7 +2,15 @@
  *  Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main (String[] args)
+	 {
+		int number = Integer.parseInt(args[0]);
+		int devisor = 1 ;
+		while (devisor<number + 1)
+		{
+			if(number % devisor == 0)
+				System.out.println(devisor);
+			devisor ++;
+		}
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,24 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
+	public static void main (String[] args)
+	{
+		String revstring = "";
+		String strorigin = args[0];
+		for(int i = strorigin.length() - 1; i > -1; i --)
+		{
+			revstring = revstring + strorigin.charAt(i);		
+		}
+
+		System.out.println(revstring);
+		System.out.println("The middle character is " + strorigin.charAt((strorigin.length()-1)/2));
+
+
+
+
+
+
+
+
 	}
 }
