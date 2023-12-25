4895483bcddee04ad8ffc4ed67051e5295f9a1d7
liram kreiner <liram.kreiner@post.runi.ac.il>
2023-12-25 18:56:58 +0200

    add inorder file

--- InOrder.java
+++ InOrder.java
@@ -3,7 +3,21 @@
  *  as long as they form a non-decreasing sequence.
  */
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
+	public static void main (String[] args) 
+	{
+		int previousnumber = (int)(Math.random()*10);
+		System.out.println(previousnumber);
+		boolean inorder = true;
+		while (inorder)
+		{
+			int number = (int)(Math.random()*10);
+			if (previousnumber<number)
+			{
+				System.out.println(number);
+				previousnumber=number;
+			}
+			else inorder=false;
+		}
+		
 	}
 }
